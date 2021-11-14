5ff07f50b6c2bd259262d516a48e3dced190b7a2
tahrer007 <tahrer_Ad@outlook.com>
2021-11-14 10:31:28 +0200

    change input

--- Ex5.6.js
+++ Ex5.6.js
@@ -1,15 +1,13 @@
 //Ex5.6 - Mask
 
-let Password = 123 ; 
-function maskPassword (Password)
-{       if (Password.length<=4) return  Password ;
-        let length = Password.length ;
-        let masked = '#'.repeat(length-4);
-        let unmasked = Password.slice(length-4, length) ; 
-        return (masked.concat(unmasked))        
- 
+let Password = "ttt";
+function maskPassword(Password) {
+  let length = Password.length;
+  if (length <= 4) return Password;
+
+  let masked = "#".repeat(length - 4);
+  let unmasked = Password.slice(length - 4, length);
+  return masked.concat(unmasked);
 }
 
-
-//console.log (maskPassword(Password+''.split(" ")));
-console.log (maskPassword(Password+""));
+console.log(maskPassword(Password + ""));
