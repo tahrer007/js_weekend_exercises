d7793ece21603e8d33f825338f52de2405589741
tahrer007 <tahrer_Ad@outlook.com>
2021-11-14 09:34:10 +0200

    ...

--- Ex5.8.js
+++ Ex5.8.js
@@ -1,30 +1,23 @@
 //Ex5.8 - shortest words version 2
 
-let string ="AAa bb  cc dd ttt SDFGDSFSDFSDF DSFDSDSFSFDSFDSFDSFAWEFDASF"; 
+let string = "AAa bb  cc dd ttt SDFGDSFSDFSDF DSFDSDSFSFDSFDSFDSFAWEFDASF";
 let strSplit = string.split(" ");
 strSplit;
 
-function findTheLongestWOrd (strSplit)
-{
-    const longestWordsArray = [];
-    let longestWordLength = 0 ; 
-    
-    strSplit.forEach(element => {
-        
-        if (element.length>longestWordLength)
-        {
-           
-           longestWordsArray.length=0;
-           longestWordsArray.push(element); 
-           longestWordLength=element.length;
-           
+function findTheLongestWOrd(strSplit) {
+  const longestWordsArray = [];
+  let longestWordLength = 0;
 
-        }
-        else if (element.length===longestWordLength) longestWordsArray.push(element);
-        
-    });
+  strSplit.forEach((element) => {
+    if (element.length > longestWordLength) {
+      longestWordsArray.length = 0;
+      longestWordsArray.push(element);
+      longestWordLength = element.length;
+    } else if (element.length === longestWordLength)
+      longestWordsArray.push(element);
+  });
 
- return longestWordsArray
+  return longestWordsArray;
 }
 
 console.log(findTheLongestWOrd(strSplit));
